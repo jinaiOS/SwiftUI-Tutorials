9dca3bd0d1cb3aeb0f0065f594fcbbba9c57189a
김지은 <kj227777@naver.com>
2022-11-02 17:09:01 +0900

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by 김지은 on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
